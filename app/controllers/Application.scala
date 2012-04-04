815e061b09de92c57bb4bf33fd013fd9b138de9d
Scott Clasen <scott@heroku.com>
2012-04-04 14:36:12 -0700

    use gaussian distribution for think time

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api.mvc._
 import play.api.libs.concurrent.Akka
-import java.util.Random
+import scala.util.Random
 import play.api.Play.current
 import java.util.concurrent.atomic.{AtomicInteger, AtomicBoolean}
 
@@ -10,7 +10,7 @@ object Application extends Controller {
 
   val failing = new AtomicBoolean(false)
   val sleep = new AtomicInteger(1)
-  val mean = 500
+  val mean = 50
   val stdDev = 1000
   val whileFailingAddPerRequest = 25
 
