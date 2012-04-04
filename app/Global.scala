60e83bed3acecb313ffa0df3661678094a1f9e5e
Scott Clasen <scott@heroku.com>
2012-04-04 10:17:20 -0700

    randomness in healthy response times, and randomness in request rate

--- app/Global.scala
+++ app/Global.scala
@@ -2,17 +2,31 @@ import play.api.libs.concurrent.Akka
 import akka.util.duration._
 import play.api.libs.ws.WS
 import play.api.{Application, GlobalSettings}
+import util.Random
 
 object Global extends GlobalSettings {
 
+  val random = new Random()
+  var appl: Application = null
 
   override def onStart(app: Application) {
-    println("scheduling self ping")
-    Akka.system(app).scheduler.schedule(100 milliseconds, 100 milliseconds) {
-      WS.url("http://failer.herokuapp.com/").get().onRedeem {
-        resp => println("got response")
+    appl = app
+    println("scheduling self pings")
+    (1 to 100) foreach {
+      _ => Akka.system(appl).scheduler.scheduleOnce(random.nextInt(500) milliseconds) {
+        ping()
+      }
+
+    }
+  }
+
+
+  private def ping() {
+    WS.url("http://failer.herokuapp.com/").get().onRedeem {
+      resp => println("got response")
+      Akka.system(appl).scheduler.scheduleOnce(random.nextInt(500) milliseconds) {
+        ping()
       }
     }
-
   }
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,23 +4,26 @@ import play.api.mvc._
 import play.api.libs.concurrent.Akka
 import java.util.Random
 import play.api.Play.current
-import java.util.concurrent.atomic.{AtomicBoolean, AtomicLong}
+import java.util.concurrent.atomic.{AtomicInteger, AtomicBoolean}
 
 object Application extends Controller {
 
   val failing = new AtomicBoolean(false)
-  val sleep = new AtomicLong(1)
+  val sleep = new AtomicInteger(1)
 
   def index = Action {
     Async {
       Akka.future {
+        var rand = new Random()
+        var think = rand.nextInt(500)
         if (failing.get()) {
           val s = sleep.get()
-          Thread.sleep(s)
           if (s < 10000) {
             sleep.addAndGet(new Random().nextInt(100))
           }
+          think += s
         }
+        Thread.sleep(think)
         Ok(views.html.index("Application status: Failing = " + failing.get()))
       }
     }
